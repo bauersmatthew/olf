c2d373a6508fa33acadf881ab982c4984acb0aaa
Matthew Bauer <bauer.s.matthew@gmail.com>
2021-09-09 12:23:34 -0700

    Add pin 53 to solenoid pins

--- controller/downstairs_pins.h
+++ controller/downstairs_pins.h
@@ -6,6 +6,7 @@ int const PIN_SOLENOID[] = {
 	45, 46, 47, 48, 49, 50,
 	42, 41, 40, 39, 38, 37,
 	37, 38, 39, 40, 41, 42,
-	7,  6,  5,  4,  3,  2
+	7,  6,  5,  4,  3,  2,
+	53
 };
-int const N_SOLENOID     = 24;
+int const N_SOLENOID     = 25;
